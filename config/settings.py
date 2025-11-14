f16cd04fd795f470a67f4aaeaafa488186618046
Alexander Averin <a.averin@rosintel.com>
2025-11-14 16:31:23 +0300

    stash API_KEY check from settings

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-API_KEY = os.getenv('API_KEY')
+YANDEX_CLOUD_FOLDER = os.getenv("YANDEX_CLOUD_FOLDER")
+YANDEX_CLOUD_API_KEY = os.getenv("YANDEX_CLOUD_API_KEY")
+# API_KEY = os.getenv('API_KEY')
 
 # Database
 DATABASE_TYPE = os.getenv('DATABASE_TYPE', 'postgresql')
@@ -17,14 +19,11 @@ DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 if not BOT_TOKEN:
     raise ValueError('BOT_TOKEN не найден в .env файле')
 
-if not API_KEY:
-    raise ValueError('API_KEY не найден в .env файле')
+# if not API_KEY:
+#     raise ValueError('API_KEY не найден в .env файле')
 
 def get_database_url() -> str:
     if DATABASE_TYPE == 'postgresql':
         return f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     else:
         return "sqlite+aiosqlite:///./quest_bot.db"
-
-YANDEX_CLOUD_FOLDER = os.getenv("YANDEX_CLOUD_FOLDER")
-YANDEX_CLOUD_API_KEY = os.getenv("YANDEX_CLOUD_API_KEY")
