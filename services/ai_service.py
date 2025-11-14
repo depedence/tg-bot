ab3186a547ca3c0d6163c734cd67ce1080b04f14
Alexander Averin <a.averin@rosintel.com>
2025-11-14 16:49:26 +0300

    last retry

--- services/ai_service.py
+++ services/ai_service.py
@@ -8,8 +8,6 @@ import os
 from config.settings import YANDEX_CLOUD_API_KEY, YANDEX_CLOUD_FOLDER
 
 # Настройки Yandex Cloud
-YANDEX_CLOUD_FOLDER = os.getenv("YANDEX_CLOUD_FOLDER")
-YANDEX_CLOUD_API_KEY = os.getenv("YANDEX_CLOUD_API_KEY")
 YANDEX_CLOUD_MODEL = "yandexgpt-lite/latest"
 
 client = openai.OpenAI(
